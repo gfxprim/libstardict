2068c88d6f025b887c8cc8057fe15bad922a7c87
Cyril Hrubis <metan@ucw.cz>
2023-02-05 15:32:59 +0100

    sd-cmd: Add an option to select dict
    Signed-off-by: Cyril Hrubis <metan@ucw.cz>

--- sd-cmd.c
+++ sd-cmd.c
@@ -1,4 +1,3 @@
-
 // SPDX-License-Identifier: LGPL-2.1-or-later
 /*
 
@@ -7,13 +6,26 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include "libstardict.h"
 
 int main(int argc, char *argv[])
 {
 	struct sd_dict_paths paths;
 	struct sd_dict *dict;
-	unsigned int i;
+	unsigned int i, d_idx = 0;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "d:")) != -1) {
+		switch (opt) {
+		case 'd':
+			d_idx = atoi(optarg);
+		break;
+		default:
+			printf("Invalid option %c\n", opt);
+		}
+	}
 
 	sd_lookup_dict_paths(&paths);
 	if (!paths.paths) {
@@ -24,12 +36,17 @@ int main(int argc, char *argv[])
 	printf("Found %u dictionaries\n", paths.dict_cnt);
 
 	for (i = 0; i < paths.dict_cnt; i++)
-		printf(" dict '%s'\n", paths.paths[i]->name);
+		printf(" %2i '%s'\n", i, paths.paths[i]->name);
 	printf("\n");
 
-	printf("Opening dict '%s'\n", paths.paths[0]->name);
+	if (d_idx >= paths.dict_cnt) {
+		printf("Dict index too large %i\n", d_idx);
+		return 1;
+	}
 
-	dict = sd_open_dict(paths.paths[0]->dir, paths.paths[0]->name);
+	printf("Opening dict '%s'\n", paths.paths[d_idx]->name);
+
+	dict = sd_open_dict(paths.paths[d_idx]->dir, paths.paths[d_idx]->name);
 	if (!dict) {
 		printf("Failed to load dict!\n");
 		return 1;
@@ -43,12 +60,12 @@ int main(int argc, char *argv[])
 
 	struct sd_lookup_res res;
 
-	if (!argv[1])
+	if (!argv[optind])
 		return 0;
 
-	printf("Lookup '%s' ... ", argv[1]);
+	printf("Lookup '%s' ... ", argv[optind]);
 
-	int ret = sd_lookup_dict(dict, argv[1], &res);
+	int ret = sd_lookup_dict(dict, argv[optind], &res);
 
 	if (!ret) {
 		printf("none\n");
@@ -60,12 +77,10 @@ int main(int argc, char *argv[])
 	printf("Result %u - %u\n", res.min, res.max);
 	printf("%s - %s\n", sd_idx_to_word(dict, res.min), sd_idx_to_word(dict, res.max));
 
-	for (unsigned int i = res.min; i <= res.max; i++) {
+	for (unsigned int i = res.min; i <= res.max; i++)
 		printf("%s\n", sd_idx_to_word(dict, i));
-	}
 
 	struct sd_entry *entry = sd_get_entry(dict, res.min);
-
 	if (entry)
 		printf("%s\n", entry->data);
 
